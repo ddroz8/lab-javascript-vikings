8ec2f9ded53578a792303d5e3ed6865b44038768
David R <dd.roz.m@gmail.com>
2022-11-11 09:59:37 +0100

    almost done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,50 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    
+    constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    }
+
+    attack(){
+    return this.strength;
+    }
+
+    receiveDamage(){
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking {
+
+    constructor (name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+        return this.health <= 0 ? `${this.name} has died in act of combat`:
+            `${this.name} has received ${damage} points of damage`;
+    }
+
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+
+    receiveDamage (damage) {
+        this.health -= damage;
+        return this.health <=0 ? "A Saxon has died in combat" : 
+            `A Saxon has received ${damage} points of damage`;
+    }
+
+}
 
 // War
 class War {}
